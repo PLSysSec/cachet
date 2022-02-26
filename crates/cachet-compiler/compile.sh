ba216e857fc2e8e791147b30f251dabf2511022e
Michael Smith <michael@spinda.net>
2022-02-25 16:04:07 -0800

    Get notes/instanceof.cachet verifying
    This requires switching to a custom version of Cachet which uses
    head-recursive loop extraction instead of tail-recursive.

--- crates/cachet-compiler/compile.sh
+++ crates/cachet-compiler/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-cargo run -- ../../notes/getprop.cachet getprop.h getprop.inc getprop.bpl
-cat ../../notes/prelude.bpl getprop.bpl | sponge getprop.bpl
+cargo run -- ../../notes/instanceof.cachet instanceof.h instanceof.inc instanceof.bpl
+cat ../../notes/prelude.bpl instanceof.bpl | sponge instanceof.bpl
