3d2a7f2c0a390bc60c3add50f42d194c6fb6db25
Michael Smith <michael@spinda.net>
2022-02-25 16:19:49 -0800

    Improve test scripts and move to repo root

--- crates/cachet-compiler/verify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-mkdir -p solve
-pushd solve >/dev/null
-../../../../corral/source/Corral/bin/Release/net5.0/corral ../instanceof.bpl /trackAllVars /recursionBound:4 || true
-popd
-rm -r solve
---
+++ scripts/compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+repo_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)
+
+notes_dir="${repo_dir}/notes"
+cachet_file="${notes_dir}/${1}.cachet"
+prelude_bpl_file="${notes_dir}/prelude.bpl"
+
+out_dir="${repo_dir}/out"
+mkdir -p "${out_dir}"
+cpp_decls_file="${out_dir}/${1}.h"
+cpp_defs_file="${out_dir}/${1}.inc"
+bpl_file="${out_dir}/${1}.bpl"
+
+cargo run --bin cachet-compiler -- \
+  "${cachet_file}" "${cpp_decls_file}" "${cpp_defs_file}" "${bpl_file}"
+cat "${prelude_bpl_file}" "${bpl_file}" | sponge "${bpl_file}"
