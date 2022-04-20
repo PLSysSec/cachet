1d9b95d8fd3860c39bd763b8b56a8d13d156894a
John Renner <john@jrenner.net>
2022-04-20 15:57:31 -0700

    Add docs to test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,9 +6,16 @@ NC='\033[0m' # No Color
 PASS="${GREEN}PASS${NC}"
 FAIL="${RED}FAIL${NC}"
 
+TMP_BPL=.test.tmp.bpl
+
+# Frontend Tests
+#
+# These are just smoke tests that don't inspect the output.
+# Currently there's no way to stop after type-checking so these tests will also detect
+# panics during code emission.
 for f in $(ls -d $repo_dir/tests/frontend/pass/*); do
     test_name="$(basename $f)...\t"
-    echo -en $test_name
+    echo -en "frontend/pass/$test_name"
     cargo run --manifest-path "$repo_dir/Cargo.toml" --quiet --bin cachet-compiler $f /dev/null /dev/null /dev/null
     if [[ $? -eq 0 ]]
     then
@@ -16,4 +23,47 @@ for f in $(ls -d $repo_dir/tests/frontend/pass/*); do
     else
         echo -e "$FAIL"
     fi
+done
+
+
+function has_bugs() {
+    if [[ "$1" == *"potential bug"* ]]
+    then
+        return 0
+    else
+        return 1
+    fi
+}
+
+# Verifier tests
+#
+# These tests run corral against the generated boogie, looking for an entrypoint called #test
+for f in $(ls -d $repo_dir/tests/verifier/pass/*); do
+    test_name="$(basename $f)...\t"
+    echo -en "verifier/pass/$test_name"
+    cargo run --manifest-path "$repo_dir/Cargo.toml" --quiet --bin cachet-compiler $f /dev/null /dev/null $TMP_BPL
+    OUT=$(corral "/main:#test" $TMP_BPL 2>&1)
+    if has_bugs "$OUT"
+    then
+        echo -e "$FAIL"
+        echo $OUT
+    else
+        echo -e "$PASS"
+    fi
+    rm $TMP_BPL
+done
+
+for f in $(ls -d $repo_dir/tests/verifier/fail/*); do
+    test_name="$(basename $f)...\t"
+    echo -en "verifier/fail/$test_name"
+    cargo run --manifest-path "$repo_dir/Cargo.toml" --quiet --bin cachet-compiler $f /dev/null /dev/null $TMP_BPL
+    OUT=$(corral "/main:#test" $TMP_BPL 2>&1)
+    if has_bugs "$OUT"
+    then
+        echo -e "$PASS"
+    else
+        echo -e "$FAIL"
+        echo $OUT
+    fi
+    rm $TMP_BPL
 done
