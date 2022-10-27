eb04011fa35c9949da642e84aaab03a0fbbf4107
Michael Smith <michael@spinda.net>
2022-10-26 21:55:50 -0700

    Fix .bpl file path in verify script

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -5,6 +5,7 @@ sample_name="${1}"
 shift
 
 scripts_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+repo_dir="$(cd -- "${scripts_dir}" &> /dev/null && cd .. && pwd)"
 bpl_file="${repo_dir}/out/${sample_name}.bpl"
 
-"${scripts_dir}/run-corral.sh" "${bpl_file}"
+"${scripts_dir}/run-corral.sh" "${bpl_file}" "${@}"
