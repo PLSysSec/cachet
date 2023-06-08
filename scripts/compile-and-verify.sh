4e47e4b03a6f916b6ba7248f029ce3342018d99d
Michael Smith <michael@spinda.net>
2023-06-08 18:32:06 +0000

    Label steps in compile-and-verify test script

--- scripts/compile-and-verify.sh
+++ scripts/compile-and-verify.sh
@@ -5,5 +5,11 @@ sample_name="${1}"
 shift
 
 scripts_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+echo Compiling ${sample_name}.cachet
+echo
 "${scripts_dir}/compile.sh" "${sample_name}"
+echo
+echo Verifying ${sample_name}.cachet
+echo
 "${scripts_dir}/verify.sh" "${sample_name}" "${@}"
