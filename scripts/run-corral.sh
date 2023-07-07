7962d676482e3dcdc2199a948dd7a14ae3be6d39
Michael Smith <michael@spinda.net>
2023-07-07 16:54:05 +0000

    Run Corral in a temporary directory
    Corral spits out junk files into the working directory, which we don't
    want polluting the tree.

--- scripts/run-corral.sh
+++ scripts/run-corral.sh
@@ -4,11 +4,16 @@ set -euo pipefail
 bpl_file="${1}"
 shift
 
+# We change directories below, so convert relative paths to absolute based on
+# the original working directory.
+bpl_file="$(realpath -s "${bpl_file}")"
+
 repo_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)"
 corral_exe="${repo_dir}/vendor/corral/source/Corral/bin/Release/net6.0/corral"
 
 # Change to a temporary directory to collect the detritus that Corral leaves behind.
 tmp_dir="$(mktemp -d)"
 trap 'rm -rf -- "${tmp_dir}"' EXIT 
+cd "${tmp_dir}"
 
-"${corral_exe}" "${bpl_file}" /trackAllVars /recursionBound:4 "${@}"
+"${corral_exe}" "${bpl_file}" /trackAllVars /recursionBound:4 /printFinalProg:/tmp/test.bpl "${@}"
