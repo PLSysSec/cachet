344bf6adf2dd1e06977e6be54d6b823b6f283516
Michael Smith <michael@spinda.net>
2024-01-29 05:24:43 +0000

    Add `run-boogie.sh` convenience script

--- scripts/run-boogie.sh
+++ scripts/run-boogie.sh
@@ -9,12 +9,12 @@ shift
 bpl_file="$(realpath -s "${bpl_file}")"
 
 repo_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)"
-corral_exe="${repo_dir}/vendor/corral/source/Corral/bin/Release/net6.0/corral"
+boogie_exe="${repo_dir}/vendor/boogie/Source/BoogieDriver/bin/Release/net6.0/BoogieDriver"
 
-# Change to a temporary directory to collect any detritus that Corral leaves
+# Change to a temporary directory to collect any detritus that Boogie leaves
 # behind.
 tmp_dir="$(mktemp -d)"
 trap 'rm -rf -- "${tmp_dir}"' EXIT 
 cd "${tmp_dir}"
 
-"${corral_exe}" "${bpl_file}" /trackAllVars /recursionBound:4 "${@}"
+"${boogie_exe}" "${bpl_file}" "${@}"
